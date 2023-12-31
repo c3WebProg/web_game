ecaa3d3598d81d1973b3de738c2ba0a3bef42e38
c3WebProg <155282528+c3WebProg@users.noreply.github.com>
2023-12-31 13:46:05 +0900

    Add files via upload

---
+++ src_game.py
@@ -0,0 +1,35 @@
+import random
+
+lb_comp = Element("lb_comp")
+
+def scissor(*args) :
+    #===============
+
+    rand()
+
+    #===============
+
+def rock(*args) :
+    #===============
+
+    rand()
+
+    #===============
+
+def paper(*args) :
+    #===============
+
+    rand()
+
+    #===============
+
+def rand (*args) :
+    
+    #=======
+
+    a = random.randrange(0,3)
+    b = ["가위", "바위", "보"]
+
+    #=======
+
+    lb_comp.elemnt.innerText = b[a]
